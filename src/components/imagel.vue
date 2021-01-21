945dca8189db20b3bec328d34d42d08224176ae2
ramisayeh <rami6sayeh@gmail.com>
2021-01-21 19:42:21 +0100

    v2

--- src/components/imagel.vue
+++ src/components/imagel.vue
@@ -3,29 +3,34 @@
     <div class="row">
       <div class="column">
         <div
-          v-for="image in images" v-bind:key="image"
-          class="imgs"
-        >
+          v-for="image in images[0]" v-bind:key="image"
+          class="imgs">
           <img  v-bind:src='image'  />
-          <button @click="deletes(item)">Delete</button>
+          <div class="overlay">
+            <button @click="deletes(item)">Delete</button>
+            </div>
         </div>
       </div>
       <div class="column">
         <div
-          v-for="image in images" v-bind:key="image"
+          v-for="image in t" v-bind:key="image"
           class="imgs"
         >
           <img  v-bind:src='image'  />
-          <button @click="deletes(item)">Delete</button>
+         <div class="overlay">
+            <button @click="deletes(item)">Delete</button>
+            </div>
         </div>
       </div>
       <div class="column">
         <div
-          v-for="image in images" v-bind:key="image"
+          v-for="image in g" v-bind:key="image"
           class="imgs"
         >
           <img  v-bind:src='image'  />
-          <button @click="deletes(item)">Delete</button>
+         <div class="overlay">
+            <button @click="deletes(item)">Delete</button>
+            </div>
         </div>
       </div>
     </div>
@@ -42,21 +47,37 @@ export default {
     },
   },
   data() {
-    return { images :[
-        'https://media.senscritique.com/media/000017453094/150_200/Sarra_Hannachi.jpg',
-        'https://i.pinimg.com/474x/8d/35/07/8d35077e43cff3cafe7843926c9567d8.jpg',
-            'https://www.jeuneafrique.com/medias/2012/03/13/012032012175333000000P.jpg',
-              'https://www.baya.tn/wp-content/uploads/2014/02/mis1.png',
-                'https://i.pinimg.com/originals/c5/1d/6b/c51d6b511edd41a278667b951baedcad.jpg',
+    return { images :[[ 'https://media.senscritique.com/media/000017453094/150_200/Sarra_Hannachi.jpg',
+        'https://i.pinimg.com/474x/8d/35/07/8d35077e43cff3cafe7843926c9567d8.jpg'],            
+        ],
+        t:['https://www.jeuneafrique.com/medias/2012/03/13/012032012175333000000P.jpg',
+              'https://www.baya.tn/wp-content/uploads/2014/02/mis1.png'],
+             g: ['https://i.pinimg.com/originals/c5/1d/6b/c51d6b511edd41a278667b951baedcad.jpg',
                   'https://i0.wp.com/arjazia.com/wp-content/uploads/2020/12/t-shirt-tunisie-femme-made-in-tunisia.jpg?fit=800%2C800&ssl=1&resize=400%2C400',
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8TPOLm2ayfQIuXBM9g51x72SCO0ul5NbDcA&usqp=CAU'
-       
-                    
-        ] }}
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8TPOLm2ayfQIuXBM9g51x72SCO0ul5NbDcA&usqp=CAU']
+                     }}
 };
 </script>
 
 <style scoped>
+.imgs:hover .overlay {
+  bottom: 0;
+  height: 100%;
+}
+.overlay {
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  right: 0;
+  background-color: #0b0d0e7c;
+  overflow: hidden;
+  width: 100%;
+  height:0;
+  transition: .5s ease;
+  border-radius: 24px;
+   margin-top: 8px;
+
+}
 .row {
   display: flex;
   flex-wrap: wrap;
@@ -67,6 +88,8 @@ export default {
   flex: 30%;
   max-width: 30%;
   padding: 0 4px;
+  justify-content:
+space-between;
 }
 .column img {
   margin-top: 8px;
@@ -87,7 +110,6 @@ button {
   line-height: 15px;
   text-transform: lowercase;
   color: #eb5757;
-  z-index: 3;
   position: absolute;
   cursor: pointer;
   top: 20px;
@@ -95,10 +117,11 @@ button {
   font-size: 18px;
   opacity: 0;
   padding: 7px;
-}
-button:hover {
-  opacity: 1;
-  border: 1px solid #cc4b4b;
+   position: absolute;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
 }
 p {
   font-style: normal;
@@ -112,13 +135,5 @@ p {
   z-index: 3;
   opacity: 0;
 }
-img:hover {
-  filter: linear-gradient(rgba(0, 0, 0, 0.6),rgba(0,0,0,0.4));
-}
-img:hover ~ button {
-  opacity: 1;
-}
-img:hover ~ p {
-  opacity: 1;
-}
+
 </style>
